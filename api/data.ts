f375d2ec1e07a2f85dd2773ae6bd0f2125c2e304
RafalPloszka <rafalploszka@live.com>
2024-04-17 23:32:46 +0200

    Fix handler

--- api/data.ts
+++ api/data.ts
@@ -2,11 +2,9 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.get(
+    const response = await axios.post(
       "https://api.tokenguard.io/db-api/growth-index/basic-timeline-data",
-      {
-        params: req.query,
-      }
+      req.body
     );
     res.json(response.data);
   } catch (error) {
